4b1367240e2cac5fcefa4b2724c3c2438501b294
Luciano Festa <lucianoefesta@hotmail.com>
2024-08-19 20:22:00 -0300

    credenciales

--- AgenciaAterrizar/Services/AuthenticatorAmadeus.cs
+++ AgenciaAterrizar/Services/AuthenticatorAmadeus.cs
@@ -3,8 +3,8 @@ using Newtonsoft.Json.Linq;
 
 public class AuthenticatorAmadeus
 {
-    private const string Client_id = "";
-    private const string Client_secret = "";
+    private const string Client_id = "qgtt7kF5CG9A1pNEZWssXpbZsqGowzT7";
+    private const string Client_secret = "QAaLCJHUcn14ntIo";
     private const string Grant_type = "client_credentials";
     private const string urlAmadeus = "https://test.api.amadeus.com/v1/security/oauth2/token";
 
